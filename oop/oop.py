060aee240bd14ba50c9160507475136afc93a44b
Orr Bar-Ilan <orr16@meet.mit.edu>
2015-01-11 19:22:45 +0200

    sleep and eat

--- oop/oop.py
+++ oop/oop.py
@@ -9,7 +9,10 @@ class Animal:
 		print(self.color)
 		print(self.age)
 		print(self.gender)
-
+	def sleep(self, where):
+		print(self.name + " is sleeping " + where)
+	def eat(self, food):
+		print(self.name + " is eating " + food)
 
 Dinosaur=Animal(name="internet_explorer", color="light blue", age=10000000000003, gender="female")
 
@@ -17,5 +20,10 @@ Dinosaur=Animal(name="internet_explorer", color="light blue", age=10000000000003
 cat=Animal(name="Fluffy", color="green", age=20, gender="not_defined")
 
 Dinosaur.print_all()
+
 print("#############")
 cat.print_all()
+print("#############")
+cat.sleep("in the sea by an island with pretty flowers")
+cat.eat(food="pizza")
+Dinosaur.eat(food="tamar")
